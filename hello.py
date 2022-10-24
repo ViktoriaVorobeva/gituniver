733c4a3a8b7eae29181505e37fad28e5d6c4d1e2
Viktoria <vorobeva.vik.99@mail.ru>
2022-10-24 17:27:56 +0300

    добавляет функцию рандомайзер

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
-print('hello, student')
+import math
+import random
+
+def getRandomPositiveInteger(a, b) :
+  if a < 0 and b < 0 :
+     print("NaN")
+     return
+
+  lower = math.ceil(min(a, b))
+  upper = math.floor(max(a, b))
+  result = random.random() * (upper - lower + 1) + lower
+  print(math.floor(result))
+  return 
+
+getRandomPositiveInteger(1,100)
